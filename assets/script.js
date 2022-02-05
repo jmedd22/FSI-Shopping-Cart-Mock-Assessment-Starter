4590bbff779450f1e30c7de791da3c1ec29dc471
Justin Meddaugh <NdoeMusic@Justins-MacBook-Pro.local>
2022-02-05 12:17:45 -0600

    added to cart logic +/- buttons

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,7 @@
-/*--- the plus and minus buttons respond to a click event (x) ---*/
-/* --- the plus button adds one to the quantity ---*/
-// the minus button subtracts one from the quantitiy
-
-// the quantity updates when the +/- buttons are clicked
-
+/*--- the plus and minus buttons respond to a click event [x] ---*/
+/* --- the plus button adds one to the quantity [x] ---*/
+/* --- the minus button subtracts one from the quantitiy [x] ---*/
+/* --- the quantity updates when the +/- buttons are clicked [] ---*/
 // The quantitiy cannot drop below 0
 
 
@@ -14,14 +12,21 @@ let rockQty = 1
 // access the buttons
 let addBtn = document.querySelector('#quantity-up')
 let minusBtn = document.querySelector('#quantity-down')
+let qtyDisplay = document.querySelector('.total-quantity')
 
 //add a click event to each button
 addBtn.addEventListener('click', function(e) {
     rockQty++
+    qtyDisplay.textContent = 'Quantity: + ${rockQty}'
 })
 
 minusBtn.addEventListener('click', function(e) {
-   rockQty--
+    if (rockQty > 1) {
+        rockQty--
+   qtyDisplay.textContent = 'Quantity: - ${rockQty}'
+
+    }
+   
 })
 
 
