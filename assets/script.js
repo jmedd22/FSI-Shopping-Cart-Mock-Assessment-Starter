6b2060c703a0b5d0bcfb66b509aead0eda7b7a4d
Justin Meddaugh <NdoeMusic@Justins-MacBook-Pro.local>
2022-02-07 16:18:44 -0600

    redid the lesson and added remove button

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,40 @@
 /* --- the plus button adds one to the quantity [x] ---*/
 /* --- the minus button subtracts one from the quantitiy [x] ---*/
 /* --- the quantity updates when the +/- buttons are clicked [] ---*/
-/* --- The quantitiy cannot drop below 0 [] ---*/
+/* --- The quantity cannot drop below 0 [] ---*/
 
 
 
 
-
-
+let rockQty = 1
 
 // access the buttons
-
+let addBtn = document.querySelector('#quantity-up')
+let minusBtn = document.querySelector('#quantity-down')
+let qtyDisplay = document.querySelector('.total-quantity')
+let removeBtn = document.querySelector('.remove')
 
 //add a click event to each button
+
+addBtn.addEventListener('click', function(e) {
+    rockQty++
+    qtyDisplay.textContent = `Quantity: ${rockQty}`
+})
+
+minusBtn.addEventListener('click', function(e) {
+    if (rockQty > 1) {
+    rockQty--
+    qtyDisplay.textContent = `Quantity: ${rockQty}`
+    }
+})
+// add remove button
+removeBtn.addEventListener('click', function(e) {
+   document.querySelector('.cart-item').remove()
+    
+})
+
+
+
 
 
 
